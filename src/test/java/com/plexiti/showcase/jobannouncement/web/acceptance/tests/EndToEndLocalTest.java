7948b9e80f2058bc4b8054e58101d7fd4e8e1ccd
Rafael Cordones Marcos <rafael.cordones@plexiti.com>
2012-11-28 13:41:28 +0100

    - Added FEST assertions - Ignore remote test for the time being

--- src/test/java/com/plexiti/showcase/jobannouncement/web/acceptance/tests/EndToEndLocalTest.java
+++ src/test/java/com/plexiti/showcase/jobannouncement/web/acceptance/tests/EndToEndLocalTest.java
@@ -1,63 +1,33 @@
 package com.plexiti.showcase.jobannouncement.web.acceptance.tests;
 
-import com.saucelabs.common.SauceOnDemandAuthentication;
-import com.saucelabs.common.SauceOnDemandSessionIdProvider;
-import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import org.junit.*;
-import org.junit.rules.TestName;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.URL;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.Date;
 
-import static junit.framework.Assert.assertTrue;
+import static org.fest.assertions.api.Assertions.assertThat;
 
-public class EndToEndTest implements SauceOnDemandSessionIdProvider {
-
-    /**
-     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
-     * supplied by environment variables or from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
-     */
-    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("plexiti", "2d003a41-7fc8-4dc3-848f-eb3fbdc8ef65");
-
-    /**
-     * JUnit Rule which will mark the Sauce Job as passed/failed when the test succeeds or fails.
-     */
-    public @Rule
-    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
-
-    /**
-     * JUnit Rule which will record the test name of the current test.  This is referenced when creating the {@link DesiredCapabilities},
-     * so that the Sauce Job is created with the test name.
-     */
-    public @Rule TestName testName= new TestName();
+public class EndToEndLocalTest {
 
     private WebDriver driver;
 
-    private String sessionId;
-
-    private String appUrl = "http://the-job-announcement.com"; // "http://localhost:8080/the-job-announcement/";
-
+    private String appUrl = "http://localhost:8080/the-job-announcement/";
 
     @Test
-    @Ignore
     public void fullProcessTest() throws Exception {
         driver.get(appUrl);
-        assertTrue(driver.getTitle().contains("Index"));
+        assertThat(driver.getTitle()).contains("Index");
 
         driver.findElement(By.linkText("Start the role play!")).click();
-        assertTrue(driver.getTitle().contains("The control center"));
+        assertThat(driver.getTitle()).contains("The control center");
 
         switchToUser("Gonzo The Great");
 
         // Click on the "Request new announcement" button
         driver.findElement(By.cssSelector("input[value='Request new announcement']")).click();
-        assertTrue(driver.getTitle().endsWith("New job anouncement"));
+        assertThat(driver.getTitle()).endsWith("New job anouncement");
 
         // Fill in the "I need" text area
         WebElement iNeedField = driver.findElement(By.tagName("textarea"));
@@ -72,7 +42,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
         driver.findElement(By.cssSelector("input[value='Request description']")).click();
 
         // Check that we are back in the list page
-        assertTrue(driver.getTitle().contains("The control center"));
+        assertThat(driver.getTitle()).contains("The control center");
 
         switchToUser("Fozzie Bear");
 
@@ -80,7 +50,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
         switchToTab("To describe");
 
         driver.findElement(By.cssSelector("input[value='Describe']")).click();
-        assertTrue(driver.getTitle().contains("Describe job announcement"));
+        assertThat(driver.getTitle()).contains("Describe job announcement");
 
         WebElement jobDescription = driver.findElement(By.cssSelector("textarea[name*='description']"));
         jobDescription.sendKeys(
@@ -108,7 +78,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
         // Click on the "Request review"
         WebElement button = driver.findElement(By.cssSelector("input[name*='review']"));
         button.click();
-        assertTrue(driver.getTitle().contains("Review job announcement"));
+        assertThat(driver.getTitle()).contains("Review job announcement");
 
         WebElement gonzoComment = driver.findElement(By.cssSelector("textarea[name*='comment']"));
         gonzoComment.sendKeys("Looks great! Publish it!");
@@ -122,7 +92,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
 
         // Click on "Publish" button
         driver.findElement(By.cssSelector("input[name*='publish']")).click();
-        assertTrue(driver.getTitle().contains("Publish job announcement"));
+        assertThat(driver.getTitle()).contains("Publish job announcement");
 
         // Click on "Publish" button
         driver.findElement(By.cssSelector("input[value='Publish']")).click();
@@ -131,7 +101,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
 
         // Click on the "View" button
         driver.findElement(By.cssSelector("input[name*='published']")).click();
-        assertTrue(driver.getTitle().contains("Published job announcement"));
+        assertThat(driver.getTitle()).contains("Published job announcement");
     }
 
     private void switchToTab(String tabTitle) {
@@ -149,22 +119,7 @@ public class EndToEndTest implements SauceOnDemandSessionIdProvider {
 
     @Before
     public void setUp() throws Exception {
-
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("platform", "Windows 2012");
-        capabilities.setCapability("version", "17");
-        capabilities.setCapability("name", testName.getMethodName());
-        capabilities.setCapability("tags", new String[]{"web", "firefox", "windows"});
-
-        this.driver = new RemoteWebDriver(
-                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
-                capabilities);
-        this.sessionId = ((RemoteWebDriver)driver).getSessionId().toString();
-    }
-
-    @Override
-    public String getSessionId() {
-        return sessionId;
+        driver = new FirefoxDriver();
     }
 
     @After
